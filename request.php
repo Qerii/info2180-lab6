d72c424176a83c050df24d1dae96178c31864a80
Topaz Grant <topaz.grant@mymona.uwi.edu>
2016-11-09 14:35:47 +0000

    XML data used to return all items

--- request.php
+++ request.php
@@ -1,8 +1,5 @@
 <?php
-
-// accept a term (keyword)
-// respond with a value
-
+/*
 $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
@@ -16,3 +13,36 @@ $definition = [
 
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+
+*/
+
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition name="definition" author="-John">
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    <definition name="bar" author="- Mary">
+        A place that sells alcholic beverages.
+    </definition>
+    <definition name="ajax" author="- Kimberly">
+        Technique which involves the use of javascript and xml (or JSON).
+    </definition>
+    <definition name="html" author="- Tenniel">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    <definition name="css" author="- Daniel">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    <definition name="javascript" author="- Karen">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    <definition name="php" author="- Jashar">
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+    </definition>
+</entries>';
+
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
+
+
